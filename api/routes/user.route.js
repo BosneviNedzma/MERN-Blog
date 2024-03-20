8cd750bf11972b917b2cb12a1cadbecee4d750d8
Nedzma Brkic <brkicnedzma@gmail.com>
2024-03-20 23:08:03 +0100

    Create get users API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getUsers,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import express from "express";
@@ -13,5 +14,6 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
