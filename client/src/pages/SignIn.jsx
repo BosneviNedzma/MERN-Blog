ac08438676a0c1f5f69d8f299ac96916406c8270
Nedzma Brkic <brkicnedzma@gmail.com>
2024-03-24 16:10:37 +0100

    Update

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -69,7 +69,7 @@ export default function SignIn() {
             Blog
           </Link>
           <p className="text-sm mt-5">
-            This is a demo project. You can sign in with your email and password
+            You can sign in with your email and password
             or with Google.
           </p>
         </div>
